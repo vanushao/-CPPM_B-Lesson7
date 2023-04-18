5c4971d1904e4dbe5e916bf66ad1b8f28550c1c5
Ivan Onegin <vanushao@gmail.com>
2023-04-18 22:02:02 +0600

    Initial commit

---
+++ Task1/Task1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+#define MODE 3
+
+#ifndef MODE
+#error Константа MODE не определена
+#endif
+
+#if MODE==1
+int add(int a, int b) {
+    return (a + b);
+}
+#endif
+
+int main() {
+    std::setlocale (LC_ALL, "Russian");
+#if MODE==0
+    std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE==1
+    std::cout << "Работаю в боевом режиме" << std::endl;
+    int a, b;
+    std::cout << "Введите число 1: ";
+    std::cin >> a;
+    std::cout << "Введите число 2: ";
+    std::cin >> b;
+    std::cout << "Результат сложения: " << add(a, b);
+#else
+    std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+#endif
+    return 0;
+}
---
+++ Task2/Task2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+#define SUB(a, b) ((a) - (b))
+
+int main() {
+	std::setlocale (LC_ALL, "Russian");
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+	return 0;
+}
